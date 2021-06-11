b1e8205f9307b6abcb75f95a5eb7590a44ccd548
MDimitri <meavilla.73@gmail.com>
2021-06-11 15:30:45 +0200

    lol

--- JS/radiation.js
+++ JS/radiation.js
@@ -24,8 +24,8 @@ function radiation() {
                 timeForAnime = 0;
                 dureeIntervale = 0;
                 interrupteur1 == 0 ? 
-                (document.body.style.backgroundImage = ('url(asset/background4.jpg)'), interrupteur1 = 1):
-                (document.body.style.backgroundImage = ('url(asset/background1.jpeg)'), interrupteur1 = 0);
+                (document.body.style.backgroundImage = ('url(/asset/background4.jpg)'), interrupteur1 = 1):
+                (document.body.style.backgroundImage = ('url(/asset/background1.jpeg)'), interrupteur1 = 0);
             }
         }
     }
