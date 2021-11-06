0a88b4eef9fb18da4bf9ef9a924cee705a2a0daa
MDimitri <meavilla.73@gmail.com>
2021-11-06 14:17:28 +0100

    k

--- JS/radiation.js
+++ JS/radiation.js
@@ -1,6 +1,5 @@
-
 //Récupération du titre
-let firstH1 = document.querySelectorAll('h1');
+let firstH1 = document.querySelectorAll("h1");
 let titrePrincipal = firstH1[0];
 // Variable global de l'animation
 let plafondIntervale = 1000 * 4;
@@ -10,45 +9,47 @@ let interrupteur1 = 0;
 let stop = false;
 //R.A.F
 function radiation() {
-    if (stop == false) {
-        dureeIntervale++;
-        if (dureeIntervale < plafondIntervale){
-            titrePrincipal.classList.remove("radiation2");
-            document.body.classList.remove("radiation");
-        
-        }else{
-            titrePrincipal.classList.add("radiation2");
-            document.body.classList.add("radiation");
-            timeForAnime++;
-            if (timeForAnime > 300) {
-                timeForAnime = 0;
-                dureeIntervale = 0;
-                interrupteur1 == 0 ? 
-                (document.body.style.backgroundImage = ('url(asset/background4.jpg)'), interrupteur1 = 1):
-                (document.body.style.backgroundImage = ('url(asset/background1.jpeg)'), interrupteur1 = 0);
-            }
-        }
+  if (stop == false) {
+    dureeIntervale++;
+    if (dureeIntervale < plafondIntervale) {
+      titrePrincipal.classList.remove("radiation2");
+      document.body.classList.remove("radiation");
+    } else {
+      titrePrincipal.classList.add("radiation2");
+      document.body.classList.add("radiation");
+      timeForAnime++;
+      if (timeForAnime > 300) {
+        timeForAnime = 0;
+        dureeIntervale = 0;
+        interrupteur1 == 0
+          ? ((document.body.style.backgroundImage =
+              "url(asset/background4.jpg)"),
+            (interrupteur1 = 1))
+          : ((document.body.style.backgroundImage =
+              "url(asset/background1.jpeg)"),
+            (interrupteur1 = 0));
+      }
     }
-    window.requestAnimationFrame(radiation);
+  }
+  window.requestAnimationFrame(radiation);
 }
 const test = window.requestAnimationFrame(radiation);
 
 // cancel anim
-const itemAnim = document.getElementById('itemAnim');
-Array.from(itemAnim.children).forEach(element => {
-    element.addEventListener("click",function () {
-        // window.cancelAnimationFrame(radiation);
-        // const test = window.requestAnimationFrame(radiation);
-        // cancelAnimationFrame(test);
-        if(stop == true) {
-            stop = false;
-            itemAnim.children[0].className = 'fas fa-times';
-            itemAnim.children[1].textContent = "stop animation";
-        }else if(stop == false){
-            stop = true;
-            itemAnim.children[0].className = 'fa fa-play';
-            itemAnim.children[1].textContent = "active animation";
-        }
-       
-    })
+const itemAnim = document.getElementById("itemAnim");
+Array.from(itemAnim.children).forEach((element) => {
+  element.addEventListener("click", function () {
+    // window.cancelAnimationFrame(radiation);
+    // const test = window.requestAnimationFrame(radiation);
+    // cancelAnimationFrame(test);
+    if (stop == true) {
+      stop = false;
+      itemAnim.children[0].className = "fas fa-times";
+      itemAnim.children[1].textContent = "stop animation";
+    } else if (stop == false) {
+      stop = true;
+      itemAnim.children[0].className = "fa fa-play";
+      itemAnim.children[1].textContent = "active animation";
+    }
+  });
 });
